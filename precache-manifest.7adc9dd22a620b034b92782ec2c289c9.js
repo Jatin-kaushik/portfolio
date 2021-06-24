3ca76b9a8fe7ce8796174adbd8642cb168d7b5d1
jatin_Byteblanket <jkaushik@byteblanket.com>
2021-06-24 20:46:35 +0530

    Updates

---
+++ precache-manifest.7adc9dd22a620b034b92782ec2c289c9.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cb5ff98a93faf48da1d6e13a3e2704",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f5b30b225d8b3609cbad",
+    "url": "/portfolio/static/css/main.7b35216c.chunk.css"
+  },
+  {
+    "revision": "3790e4cefde589bbfbf4",
+    "url": "/portfolio/static/js/2.3e1fe52b.chunk.js"
+  },
+  {
+    "revision": "f5b30b225d8b3609cbad",
+    "url": "/portfolio/static/js/main.4dbe17c0.chunk.js"
+  },
+  {
+    "revision": "ee785d5981467b76a40e",
+    "url": "/portfolio/static/js/runtime-main.26dee75f.js"
+  },
+  {
+    "revision": "ccdf776368c8e5c3e91512581cae1a6c",
+    "url": "/portfolio/static/media/20491-signature.ccdf7763.gif"
+  },
+  {
+    "revision": "9bcd234dc44cb3ce7684c2e406795b5a",
+    "url": "/portfolio/static/media/21472-code-debugging.9bcd234d.gif"
+  },
+  {
+    "revision": "599e7730418bb3ab7f6028c7faed9c0a",
+    "url": "/portfolio/static/media/36165-dashboard.599e7730.gif"
+  },
+  {
+    "revision": "7ffb9282826c2c46e35a3e1eafc02e62",
+    "url": "/portfolio/static/media/45586-dashboard-01.7ffb9282.gif"
+  },
+  {
+    "revision": "31741c69dfb25ea5d6504be4e2e8f559",
+    "url": "/portfolio/static/media/47155-coding.31741c69.gif"
+  },
+  {
+    "revision": "b515f003015896364795dd5662e39a95",
+    "url": "/portfolio/static/media/50834-dashboard.b515f003.gif"
+  },
+  {
+    "revision": "b6653e3893aace32acc8f3ec0565c141",
+    "url": "/portfolio/static/media/56922-code-typing-concept.b6653e38.gif"
+  },
+  {
+    "revision": "08926bad354c71a17addc48fc5e166a6",
+    "url": "/portfolio/static/media/61918-code-animation.08926bad.gif"
+  },
+  {
+    "revision": "e4fd16f975193bc01fbd308f54478253",
+    "url": "/portfolio/static/media/64936-smiley-face-cartoon.e4fd16f9.gif"
+  },
+  {
+    "revision": "604d5fc81aa23691ccbba2ce04b2146b",
+    "url": "/portfolio/static/media/8683-face-scanning.604d5fc8.gif"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "b8349593342654d8ca5686e6cb73f13b",
+    "url": "/portfolio/static/media/Code.b8349593.ttf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c0c6a34992de89a9c84f0d2e6550c05e",
+    "url": "/portfolio/static/media/Relu-Activation-Function.c0c6a349.gif"
+  },
+  {
+    "revision": "5eae86485d4f6947c36071cbd15b364a",
+    "url": "/portfolio/static/media/acti.5eae8648.gif"
+  },
+  {
+    "revision": "5c232fa18ee1828e7bd6ff8de497f4e2",
+    "url": "/portfolio/static/media/activation-functions.5c232fa1.gif"
+  },
+  {
+    "revision": "77f03470d190d5fd26ba46f1d67e977a",
+    "url": "/portfolio/static/media/animation_640_kqar8i2w.77f03470.gif"
+  },
+  {
+    "revision": "9eca5b7835f502441ae8a439903e95f1",
+    "url": "/portfolio/static/media/animation_640_kqarl2sc.9eca5b78.gif"
+  },
+  {
+    "revision": "1a58a3f34fa43db27bc8b56cc191614b",
+    "url": "/portfolio/static/media/coursera-01.1a58a3f3.png"
+  },
+  {
+    "revision": "cf3766d93adba4cbd9ecf8c46485245c",
+    "url": "/portfolio/static/media/data.cf3766d9.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/portfolio/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/portfolio/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/portfolio/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "672ebc9c0cbca3d1bcf667a6c1cd6638",
+    "url": "/portfolio/static/media/hck.672ebc9c.png"
+  },
+  {
+    "revision": "ccb710b7462e7dc63ecd8aff587374ac",
+    "url": "/portfolio/static/media/iitg-01.ccb710b7.png"
+  },
+  {
+    "revision": "d77d193283c796188a0576dcf6bdcf00",
+    "url": "/portfolio/static/media/ineuron-01.d77d1932.png"
+  },
+  {
+    "revision": "5aa6bdcc131e211e4b7f4aa77934ca68",
+    "url": "/portfolio/static/media/neural.5aa6bdcc.gif"
+  },
+  {
+    "revision": "4f42a4cb6130711885ca92e07912dc0d",
+    "url": "/portfolio/static/media/news.4f42a4cb.gif"
+  },
+  {
+    "revision": "e19e9a690c9506606a6035dcf93ea524",
+    "url": "/portfolio/static/media/problem-01.e19e9a69.png"
+  },
+  {
+    "revision": "1a47ef1472ad2ec430794b554838afed",
+    "url": "/portfolio/static/media/project-01.1a47ef14.png"
+  },
+  {
+    "revision": "8e3b082656d1a1cfda0ae48afff053f0",
+    "url": "/portfolio/static/media/project-04.8e3b0826.png"
+  },
+  {
+    "revision": "0a4c75215cc780bf332274dd8135679e",
+    "url": "/portfolio/static/media/project-05.0a4c7521.png"
+  },
+  {
+    "revision": "6daaa585fb55c736a3ed2e0ade5da93d",
+    "url": "/portfolio/static/media/project-06.6daaa585.png"
+  },
+  {
+    "revision": "efb76d2072a212a43ccbb90c4ccd4c78",
+    "url": "/portfolio/static/media/project-07.efb76d20.png"
+  },
+  {
+    "revision": "6369a07cb433a56e786881dc29d3536a",
+    "url": "/portfolio/static/media/python-01.6369a07c.png"
+  },
+  {
+    "revision": "d2641629f3a39c0bbb0e502cb1274e9c",
+    "url": "/portfolio/static/media/simplilearn-01.d2641629.png"
+  },
+  {
+    "revision": "bb24e623f32da61e6c1278add6ec3161",
+    "url": "/portfolio/static/media/tabana-01.bb24e623.png"
+  },
+  {
+    "revision": "f59dceabccf06d52160e2b7e68bf985f",
+    "url": "/portfolio/static/media/tabcon-01.f59dceab.png"
+  },
+  {
+    "revision": "1de5364c67dd60e0d0aa9567f207ae87",
+    "url": "/portfolio/static/media/udemy-01.1de5364c.png"
+  },
+  {
+    "revision": "97948ca733915eb236871a1b422e3a2b",
+    "url": "/portfolio/static/media/voice.97948ca7.gif"
+  },
+  {
+    "revision": "9331f9728bff50b3d6b330f7d5d6bc51",
+    "url": "/portfolio/static/media/web.9331f972.svg"
+  }
+]);
